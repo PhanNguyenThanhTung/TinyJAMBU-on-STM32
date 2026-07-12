fe24aae68db28eaa8fd3f787d9ac076e483078d3
Thanh Tung <156272078+PhanNguyenThanhTung@users.noreply.github.com>
2026-07-12 20:34:33 +0700

    define status, callback function for uart and buffer

---
+++ stm32_sender/include/project_types.h
@@ -0,0 +1,24 @@
+#ifndef PROJECT_TYPES_H
+#define PROJECT_TYPES_H
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef enum {
+    STATUS_OK = 0,
+    STATUS_ERROR,
+    STATUS_TIMEOUT,
+    STATUS_BUSY,
+    STATUS_INVALID_PARAMETER,
+    STATUS_BUFFER_OVERFLOW,
+    STATUS_AUTHENTICATION_FAILED
+} status_t;
+
+typedef struct {
+    uint8_t * data;
+    uint32_t length;
+} buffer_t;
+
+typedef void (*callback_t)(void);
+
+#endif
