07ca83009d3d9692371dfa4aa212f203d6aff56f
Thanh Tung <156272078+PhanNguyenThanhTung@users.noreply.github.com>
2026-07-14 03:22:30 +0700

    add description

--- stm32_sender/include/project_config.h
+++ stm32_sender/include/project_config.h
@@ -1,21 +1,22 @@
 #ifndef PROJECT_CONFIG_H
 #define PROJECT_CONFIG_H
-
+/* System clock */
 #define HSE_CLOCK_HZ               8000000UL
 #define SYSTEM_CLOCK_HZ           72000000UL
-
+/* UART */
 #define UART_BAUD_RATE             115200UL
 #define UART_TX_BUFFER_SIZE        256U
 #define UART_RX_BUFFER_SIZE        256U
-
+/* UART tranfer config */
 #define FRAME_START_BYTE           0xA5U
-#define MAX_PAYLOAD_SIZE           128U
-
+#define MAX_PAYLOAD_SIZE           128U /* TiniJAMBE 128-bit */
+/* TinyJAMBU config  */
 #define KEY_SIZE_BYTES             16U
 #define NONCE_SIZE_BYTES           12U
 #define TAG_SIZE_BYTES             8U
-
-#define WATCHDOG_TIMEOUT_MS        2000U
+/* Watchdog config */
+#define WATCHDOG_TIMEOUT_MS        2000U /* Timeout 2s */
+/* Data config */
 #define DATA_SEND_PERIOD_MS        1000U
 
 #endif 
