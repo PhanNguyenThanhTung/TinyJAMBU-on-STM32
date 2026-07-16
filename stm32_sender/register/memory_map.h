af5847181007c712ddb9dd607362247455f4d56b
Thanh Tung <156272078+PhanNguyenThanhTung@users.noreply.github.com>
2026-07-17 02:07:39 +0700

    add Peripheral base addresses

---
+++ stm32_sender/register/memory_map.h
@@ -0,0 +1,13 @@
+#ifndef MEMORY_MAP_H
+#define MEMORY_MAP_H
+
+/* Peripheral Base Addresses */ 
+#define RCC_BASE          0x40021000UL
+#define FLASH_BASE        0x40022000UL
+#define GPIOA_BASE        0x40010800UL
+#define GPIOB_BASE        0x40010C00UL
+#define GPIOC_BASE        0x40011000UL
+#define USART1_BASE       0x40013800UL
+#define IWDG_BASE         0x40003000UL
+
+#endif
