3665666acb91d927b42dbc9aadee3ba31954ef2a
Thanh Tung <156272078+PhanNguyenThanhTung@users.noreply.github.com>
2026-07-18 02:28:19 +0700

    complete nvic register

--- stm32_sender/register/nvic_register.h
+++ stm32_sender/register/nvic_register.h
@@ -4,7 +4,31 @@
 #include <stdint.h>
 #include "memory_map.h"
 
+typedef struct {
+    /* Interrupt Set-Enable Registers (Offset: 0x000 - 0x008) */
+    volatile uint32_t ISER[3];
+    /* Reserved space (Offset: 0x00C - 0x07C) */
+    volatile uint32_t RESERVED0[29];
+    /* Interrupt Clear-Enable Registers (Offset: 0x080 - 0x088) */
+    volatile uint32_t ICER[3];
+    /* Reserved space (Offset: 0x08C - 0x0FC) */
+    volatile uint32_t RESERVED1[29];
+    /* Interrupt Set-Pending Registers (Offset: 0x100 - 0x108) */
+    volatile uint32_t ISPR[3];
+    /* Reserved space (Offset: 0x10C - 0x17C) */
+    volatile uint32_t RESERVED2[29];
+    /* Interrupt Clear-Pending Registers (Offset: 0x180 - 0x188) */
+    volatile uint32_t ICPR[3];
+    /* Reserved space (Offset: 0x18C - 0x1FC) */
+    volatile uint32_t RESERVED3[29];
+    /* Interrupt Active Bit Registers (Offset: 0x200 - 0x208) */
+    volatile uint32_t IABR[3];
+    /* Reserved space (Offset: 0x20C - 0x2FC) */
+    volatile uint32_t RESERVED4[61];
+    /* Interrupt Priority Registers (Offset: 0x300 - 0x353) */
+    volatile uint32_t IPR[21];   
+} nvic_register_t;
 
-
+#define NVIC ((nvic_register_t *)NVIC_BASE)
 
 #endif
