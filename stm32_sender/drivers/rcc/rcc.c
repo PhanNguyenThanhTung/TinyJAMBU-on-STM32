f880b0acc5a2a65eaca2cd565c61a9b768d9f71c
Thanh Tung <156272078+PhanNguyenThanhTung@users.noreply.github.com>
2026-08-03 18:36:51 +0700

    add status check for status function

--- stm32_sender/drivers/rcc/rcc.c
+++ stm32_sender/drivers/rcc/rcc.c
@@ -13,15 +13,37 @@ static status_t rcc_enable_pll(void);
 static status_t rcc_switch_system_clock_to_pll(void);
 
 status_t rcc_init() {
+    status_t status;
     rcc_enable_hse();
+    status = rcc_enable_hse();
+
+    if (status != STATUS_OK) {
+        return status;
+    }
+
     rcc_configure_flash();
     rcc_configure_bus_prescalers();
     rcc_configure_pll();
     rcc_enable_pll();
-    rcc_switch_system_clock_to_pll();
+    status = rcc_enable_pll();
 
+    if (status != STATUS_OK) {
+        return status;
+    }
+
+    rcc_switch_system_clock_to_pll();
+    status = rcc_switch_system_clock_to_pll();
+
+    if (status != STATUS_OK) {
+        return status;
+    }
+    
     system_core_clock_hz = 36000000UL;
     return STATUS_OK;
 }
 
-static void rcc_configure_flash(void);
+static status_t rcc_enable_hse(void) {
+    uint32_t timeout = 5000UL;
+
+    while(!(RCC->CR & ())
+}
