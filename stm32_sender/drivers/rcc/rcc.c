07edf89e748bcd774e9f0d39d065da7e04d283d7
Thanh Tung <tungp5656@gmail.com>
2026-08-04 22:10:42 +0700

    Write the necessary RCC functions fir the project

--- stm32_sender/drivers/rcc/rcc.c
+++ stm32_sender/drivers/rcc/rcc.c
@@ -43,14 +43,67 @@ status_t rcc_init() {
 static status_t rcc_enable_hse(void) {
     uint32_t timeout = 5000UL;
 
-    RCC->CR != (1 << 16);
+    RCC->CR |= (1 << 16);
 
     while(!(RCC->CR & (1 << 17))) {
-        if(timeout <= 0) {
+        if(timeout--== 0) {
             return STATUS_TIMEOUT;
         }
-        timeout--;
     }
     return STATUS_OK;
 }
 
+static void rcc_configure_flash() {
+    FLASH->ACR &= ~0x07; /*Clear bit 2:*/
+    FLASH->ACR |= 0x01; /*Set Latency to 1 wait state*/
+    FLASH->ACR |= (1 << 4);
+}
+
+static void rcc_configure_bus_prescalers(void) {
+    RCC->CFGR &= ~(0x3FF << 4);
+}
+
+static void rcc_configure_pll(void) {
+    RCC->CFGR &= ~(0x3F << 16);
+    RCC->CFGR |= (1 << 16);
+    RCC->CFGR |= (1 << 17);
+    RCC->CFGR |= (0b0111 << 18);
+}
+
+static status_t rcc_enable_pll(void) {
+    uint32_t timeout = 5000UL;
+    RCC->CR |= (1 << 24);
+
+    while(!(RCC->CR & (1 << 25))) {
+        if(timeout-- == 0) {
+            return STATUS_TIMEOUT;
+        }
+    }
+    return STATUS_OK;
+}
+
+static status_t rcc_switch_system_clock_to_pll(void) {
+    uint32_t timeout = 5000UL;
+    RCC->CFGR &= ~(0b11);
+    RCC->CFGR |= 0x2; /*0b10*/
+
+    while((RCC->CFGR & (0x3 << 2)) != (0x2 << 2)) {
+        if(timeout-- == 0) {
+            return STATUS_TIMEOUT;
+        }
+    }
+    return STATUS_OK;
+}
+
+void rcc_enable_gpioa_clock(void) {
+    RCC->APB2ENR |= (1 << 2);
+}
+void rcc_enable_gpiob_clock(void) {
+    RCC->APB2ENR |= (1 << 3);
+}
+void rcc_enable_gpioc_clock(void) {
+    RCC->APB2ENR |= (1 << 4);
+}
+void rcc_enable_usart1_clock(void) {
+    RCC->APB2ENR |= (1 << 14);
+}
